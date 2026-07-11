fb99b3ac0dd956ecefdc93a14c58cfa55832b031
govindsarang <govindsarang02@gmail.com>
2026-07-12 00:26:00 +0530

    Time: 248 ms (99.83%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
-Select max(e1.salary) as SecondHighestSalary
-from Employee e1 inner join employee e2 
-on e1.salary<e2.salary
+#Select max(e1.salary) as SecondHighestSalary
+#rom Employee e1 inner join employee e2 
+#on e1.salary<e2.salary
+select ( Select
+distinct salary  from Employee 
+order by salary desc 
+limit 1 offset 1 
+) as SecondHighestSalary
+
