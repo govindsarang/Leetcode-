73e19ba31eed396ead67f1ee416a98b9eb8d64e5
govindsarang <govindsarang02@gmail.com>
2026-07-16 12:33:10 +0530

    Time: 376 ms (46.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,24 @@
+SELECT visited_on,
+(
+    SELECT SUM(amount)
+    FROM Customer
+    WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY)
+                        AND c.visited_on
+) AS amount,
+ROUND(
+(
+    SELECT SUM(amount)
+    FROM Customer
+    WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY)
+                        AND c.visited_on
+)/7,2) AS average_amount
+FROM
+(
+    SELECT DISTINCT visited_on
+    FROM Customer
+) c
+WHERE visited_on >= (
+    SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
+    FROM Customer
+)
+ORDER BY visited_on;
