d94e7013bd8b20314147e321e9e887180826fef0
govindsarang <govindsarang02@gmail.com>
2026-07-25 23:29:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (54.97%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
@@ -5,9 +5,7 @@ class Solution:
         for i in range(len(n)):
             arr.append(int(n[i]))
         arr.sort()
-        max1=arr[-1]
-        max2=arr[-2]
-        return max1*max2
+        return arr[-1]*arr[-2]
 
 
         
