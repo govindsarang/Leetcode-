2033b076e2e934833b9c79c16fa77f918fe734c6
govindsarang <govindsarang02@gmail.com>
2026-08-18 10:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (16.12%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,32 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        positive=True
+        s=s.strip()
+        num=0
+        if len(s)==0:
+            return 0
+        if s[0]=="-":
+            positive=False
+            s=s[1:]
+        elif s[0]=="+":
+            s=s[1:]    
+        for i in range(len(s)):
+            if s[i].isdigit():
+                digit=ord(s[i])-ord('0')
+                num=num*10+digit
+            else:
+                break
+        if positive==False:
+            num=-num
+        if num > 2147483647:
+            return 2147483647
+
+        if num < -2147483648:
+            return -2147483648
+        return num
+        
+
+
+        
+
+        
